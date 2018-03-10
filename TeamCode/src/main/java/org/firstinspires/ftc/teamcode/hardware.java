2480a90fdd53475997022e59335c7005c0f21e92
anaisnothere <34169825+anaisnothere@users.noreply.github.com>
2018-03-10 23:11:01 +0200

    mi-e somn

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware.java
@@ -1,8 +1,14 @@
 package org.firstinspires.ftc.teamcode;
 
+import android.graphics.Color;
+
+import com.qualcomm.hardware.modernrobotics.ModernRoboticsI2cColorSensor;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.ColorSensor;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
 /**
  * Created by Ana on 2/11/2018.
@@ -10,16 +16,20 @@ import com.qualcomm.robotcore.hardware.Servo;
 
 public class hardware {
 
-    public DcMotor lbDrive      = null;
-    public DcMotor rbDrive      = null;
-    public DcMotor lfDrive      = null;
-    public DcMotor rfDrive      = null;
-    public Servo   lclaw        = null;
-    public Servo   rcalw        = null;
-
+    public DcMotor lbDrive = null;
+    public DcMotor rbDrive = null;
+    public DcMotor lfDrive = null;
+    public DcMotor rfDrive = null;
+    public DcMotor armDrive = null;
+    public Servo sl = null;
+    public Servo sr = null;
+    //public Servo   sb        = null;
+    public CRServo cr = null;
+    public ColorSensor colorSensor;
 
     /* local OpMode members. */
-    HardwareMap hwMap         = null;
+    HardwareMap hwMap;
+//    private ElapsedTime period = new ElapsedTime();
 
     public void Hardware() {
 
@@ -30,38 +40,55 @@ public class hardware {
         // Save reference to Hardware map
         hwMap = ahwMap;
 
+        colorSensor = hwMap.get(ColorSensor.class, "color");
+        colorSensor.enableLed(false);
         // Define and Initialize Motors
-        lbDrive  = hwMap.get(DcMotor.class, "lbDrive");
-        rbDrive = hwMap.get(DcMotor.class, "rbDrive");
-        lfDrive = hwMap.get(DcMotor.class, "lfDrive");
-        rfDrive = hwMap.get(DcMotor.class, "rfDrive");
+        lbDrive = hwMap.get(DcMotor.class, "lb");
+        rbDrive = hwMap.get(DcMotor.class, "rb");
+        lfDrive = hwMap.get(DcMotor.class, "lf");
+        rfDrive = hwMap.get(DcMotor.class, "rf");
+        armDrive = hwMap.get(DcMotor.class, "glis");
 
-        lbDrive.setDirection(DcMotor.Direction.REVERSE);
-        rbDrive.setDirection(DcMotor.Direction.FORWARD);
+        lbDrive.setDirection(DcMotor.Direction.FORWARD);
         lfDrive.setDirection(DcMotor.Direction.REVERSE);
+        rbDrive.setDirection((DcMotor.Direction.REVERSE));
         rfDrive.setDirection(DcMotor.Direction.FORWARD);
+
+        armDrive.setDirection(DcMotor.Direction.FORWARD);
 
         // Set all motors to zero power
         lbDrive.setPower(0);
         rbDrive.setPower(0);
         lfDrive.setPower(0);
         rfDrive.setPower(0);
+        armDrive.setPower(0);
 
         // Set all motors to run without encoders.
         // May want to use RUN_USING_ENCODERS if encoders are installed.
-        lbDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        rbDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        lfDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        rfDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        // lbDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        //  rbDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        //  lfDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        // rfDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        //  armDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 
-        lbDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        rbDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        lfDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        rfDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        // lbDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        // rbDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        //  lfDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        // rfDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        //armDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
         // Define and initialize ALL installed servos.
-        lclaw = hwMap.get(Servo.class, "claw1");
-        rcalw = hwMap.get(Servo.class, "claw2");
+        sl = hwMap.get(Servo.class, "s1");
+        sr = hwMap.get(Servo.class, "s2");
+        //sb = hwMap.get(Servo.class, "s3");
+        cr = hwMap.crservo.get("crservo");
+        cr.setPower(0);
+
+
+        sr.setDirection(Servo.Direction.REVERSE);
+        //sb.setPosition(1); //!!!!!!!!
+
+
     }
 }
 
